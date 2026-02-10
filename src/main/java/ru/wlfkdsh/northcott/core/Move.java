ba00697a79c30127b5ea398fa7f4c70f5f694bf8
Sergey Lipin <ivashka.tester@yandex.ru>
2026-02-10 23:59:16 +1000

    Refactoring

--- src/main/java/ru/wlfkdsh/northcott/core/Move.java
+++
@@ -1,9 +0,0 @@
-package ru.wlfkdsh.northcott.core;
-
-public record Move(Player player, int row, int newCol) {
-    public Move {
-        if (player == null) throw new IllegalArgumentException("player == null");
-        if (row < 0) throw new IllegalArgumentException("row < 0");
-        if (newCol < 0) throw new IllegalArgumentException("newCol < 0");
-    }
-}
